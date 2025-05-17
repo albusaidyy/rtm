52f6d87e1f12ce452cf06a945e78a6166f53ef9b
albusaidyy <albusaidy05@gmail.com>
2025-05-17 15:12:19 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/albusaidy/org/rtm/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.albusaidy.org.rtm
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:rtm/app/app.dart';
+import 'package:rtm/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
