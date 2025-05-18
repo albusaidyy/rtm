0f2949b45f31432e4cddbd0ce796686041f328e6
albusaidyy <albusaidy05@gmail.com>
2025-05-18 18:25:44 +0300

    Add service class

--- lib/features/visit_tracker/data/services/visit_service.dart
+++ lib/features/visit_tracker/data/services/visit_service.dart
@@ -1,14 +1,28 @@
 import 'package:rtm/features/visit_tracker/visits/data/models/visit.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 abstract class VisitService {
   Future<Visits> getVisits();
+  Future<void> createVisit(Visit visit);
 }
 
 class VisitServiceImpl implements VisitService {
+  final supabase = Supabase.instance.client;
+
   @override
   Future<Visits> getVisits() async {
     try {
-      return Visits(visits: []);
+      final response = await supabase.from('visits').select();
+      return Visits(visits: response.map(Visit.fromJson).toList());
+    } on Exception catch (_) {
+      rethrow;
+    }
+  }
+
+  @override
+  Future<void> createVisit(Visit visit) async {
+    try {
+      await supabase.from('visits').insert(visit.toJson());
     } on Exception catch (_) {
       rethrow;
     }
